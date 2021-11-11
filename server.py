d3e41ec12a07c2c6fdf8d5410a9f52ccac8e494a
MikeMarshall36 <90028680+MikeMarshall36@users.noreply.github.com>
2021-11-11 20:40:03 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+sock.bind(('', 9091))
+sock.listen(2)
 
 msg = ''
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+while msg != "exit":
+	conn, addr = sock.accept()
+	print("Connected: ", addr)
+	msg = ''
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		msg += data.decode()
+		conn.send(data)
+	print(msg)
 
-print(msg)
 
 conn.close()
